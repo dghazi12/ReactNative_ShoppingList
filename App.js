c9a266fcc5a0124392e5c1fc654ed9d3763a57cf
David Ghazi <davidaghazi@gmail.com>
2020-08-20 21:31:43 -0400

    completed shopping list

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import Header from './components/Header';
+import { uuid } from 'uuidv4';
 
 const App = () => {
+
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'}
+    {id: uuid(), text: 'Egg'}
+    {id: uuid(), text: 'Bread'}
+    {id: uuid(), text: 'Juice'}
+  ]);
+
   return(
     <View style={styles.container}>
       <Header />
